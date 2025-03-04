b970d48378ac305436f9319bf138ed9cce4a5e66
Sai shruthi Haridasu <138649324+Shruthi6641@users.noreply.github.com>
2025-03-05 00:20:33 +0530

    Add files via upload

---
+++ jsday11.js
@@ -0,0 +1,62 @@
+let ch1=document.getElementsByClassName("child1")
+ch1[0].style.textAlign="center"
+ch1[0].style.fontStyle="italic"
+let ch2=document.getElementsByClassName("child2")
+ch2[0].style.textAlign="center"
+// ch2[0].style.textDecoration="none"
+let ch3=document.getElementsByClassName("child3")
+ch3[0].style.textAlign="center"
+let car=document.getElementById("career")
+car.style.color="purple"
+let ch4=document.getElementsByClassName("child4")
+for(i=0;i<ch4.length;i++){
+    ch4[i].style.color="red"
+}
+let edu=document.getElementById("ed")
+edu.style.color="blue"
+let ch5=document.getElementsByClassName("child5")
+ch5[0].style.color="orange"
+let year1=document.getElementById("year1")
+year1.style.float="right"
+let year2=document.getElementById("year2")
+year2.style.float="right"
+let tech=document.getElementById("ts")
+tech.style.color="green"
+let techskill=document.getElementsByClassName("tskill")
+for(i=0;i<techskill.length;i++){
+    techskill[i].style.color="lightblue"
+}
+let behave=document.getElementById("bs")
+behave.style.color="yellow"
+let beskill=document.getElementsByClassName("bskills")
+for(i=0;i<beskill.length;i++){
+    beskill[i].style.color="pink"
+
+}
+let certi=document.getElementById("certi")
+certi.style.color="grey"
+let certis=document.getElementsByClassName("certis")
+for(i=0;i<certis.length;i++){
+    certis[i].style.color="blue"
+}
+let pro=document.getElementById("pro")
+pro.style.color="aqua"
+let head=document.getElementsByTagName("h3")
+for(i=0;i<head.length;i++){
+    head[i].style.color="brown"
+    head[i].style.fontStyle="italic"
+}
+let pro1=document.getElementsByClassName("pro1")
+for(i=0;i<pro1.length;i++){
+    pro1[i].style.color="deeppink"
+}
+let pro2=document.getElementsByClassName("pro2")
+    for(i=0;i<pro2.length;i++){
+        pro2[i].style.color="darkslategray"
+    }
+let ac=document.getElementById("ac")
+ac.style.color="crimson"
+let acti=document.getElementsByClassName("acti")
+for(i=0;i<acti.length;i++){
+    acti[i].style.color="aquamarine"
+}
